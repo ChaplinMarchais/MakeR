22cba0abecfa58d8ffba8cbcf1b87cc661381f42
Chaplin Marchais <chap.m.gigga@gmail.com>
2025-12-12 15:38:34 -0800

    Refactor find_stage_quantities for style

--- MakeR/R/analytics.R
+++ MakeR/R/analytics.R
@@ -7,8 +7,8 @@ find_avg_stage_time <- function(item_data, stage_time_postfix = "_Hours") {
       dplyr::summarise(dplyr::across(
         dplyr::ends_with(stage_time_postfix),
         ~ mean(.x, na.rm = TRUE)
-    ),
-    .by = item_col)
+      ), .by = item_col) |>
+      dplyr::ungroup()
 
     result
 }
@@ -18,14 +18,11 @@ find_stage_quantities <- function(item_data, stage_col = "Current_Stage", qty_co
     item_col <- attr(item_data, "item_id_col", exact = TRUE)
 
     result <- item_data |>
-      dplyr::select(item_col,
-                    stage_col,
-                    dplyr::ends_with(qty_col)) |>
-      dplyr::group_by(Item_ID = item_data[[item_col]], Stage = item_data[[stage_col]]) |>
-      dplyr::summarise(dplyr::across(
-        qty_col,
-        ~ sum(.x, na.rm = TRUE)
-    )) |>
+      dplyr::select(item_col, stage_col, qty_col) |>
+      dplyr::group_by(Item_ID = item_data[[item_col]],
+                      Stage = item_data[[stage_col]]) |>
+      dplyr::summarise(dplyr::across(qty_col,
+                                     ~ sum(.x, na.rm = TRUE))) |>
       dplyr::ungroup()
 
     result
