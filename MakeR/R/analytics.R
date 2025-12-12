5e9434a2535a8affff6709be9b6f8d47b386aff1
Chaplin Marchais <chap.m.gigga@gmail.com>
2025-12-12 15:32:01 -0800

    Add find_stage_quantities() to return the total quantitiy of each item in each stage of production.

--- MakeR/R/analytics.R
+++ MakeR/R/analytics.R
@@ -12,3 +12,21 @@ find_avg_stage_time <- function(item_data, stage_time_postfix = "_Hours") {
 
     result
 }
+
+#' @export
+find_stage_quantities <- function(item_data, stage_col = "Current_Stage", qty_col = "Quantity") {
+    item_col <- attr(item_data, "item_id_col", exact = TRUE)
+
+    result <- item_data |>
+      dplyr::select(item_col,
+                    stage_col,
+                    dplyr::ends_with(qty_col)) |>
+      dplyr::group_by(Item_ID = item_data[[item_col]], Stage = item_data[[stage_col]]) |>
+      dplyr::summarise(dplyr::across(
+        qty_col,
+        ~ sum(.x, na.rm = TRUE)
+    )) |>
+      dplyr::ungroup()
+
+    result
+}
